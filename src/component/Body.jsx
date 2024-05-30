e80f79eaddc4ea6ba2b5b92b454e9c2efef44cdc
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-05-30 16:16:07 +0530

    update

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,47 +1,27 @@
 import Card from "./Card";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-  let listOfRest = [
-    {
-      info: {
-        id: "636723",
-        name: "Chinese Wok",
-        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-        costForTwo: "₹250 for two",
-        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-        avgRating: 3.9,
-        sla: {
-          deliveryTime: 32,
-          slaString: "30-35 mins",
-        },
-      },
-    },
-    {
-      info: {
-        id: "636724",
-        name: "Chinese Wok",
-        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
-        costForTwo: "₹250 for two",
-        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
-        avgRating: 4.2,
-        sla: {
-          deliveryTime: 32,
-          slaString: "30-35 mins",
-        },
-      },
-    },
-  ];
+  let [listOfRest, setlistOfRest] = useState(resList);
   return (
     <div className="body">
       <div className="btn">
         <button
           className="filter-btn"
           onClick={() => {
-            // console.log(resList.info.avgRating);
-            listOfRest.filter((rest) => rest.avgRating < 4);
-            console.log(listOfRest);
-          }}
+            // console.log(listOfRest[0].info.avgRating);
+          //  listOfRest.filter((rest) => );
+            const filteredList = listOfRest.filter((rest) => rest.info.avgRating > 4.2);
+            // resList = resList.filter((rest) => rest.info.avgRating > 4.9);
+            // console.log(resList[0].info.avgRating)
+            // console.log(resList)
+            
+           setlistOfRest(filteredList)
+          }
+         
+
+          }
         >
           Top Rated Resturant
         </button>
--- src/utils/mockData.jsx
+++ src/utils/mockData.jsx
@@ -1,4 +1,4 @@
-const resList = [
+let resList = [
   {
     "info": {
       "id": "636723",
